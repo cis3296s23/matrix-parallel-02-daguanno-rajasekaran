ad380aa5e7e388221b68901c76a1c60bd7bc68c3
Raj <alexander.rajasekaran@gmail.com>
2023-03-16 15:55:54 -0400

    changes to main to make testing individual approaches easier

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "mmult_simd.c"
 #include "mmult.c"
@@ -13,15 +14,19 @@
 int main(int argc, char *argv[]) {
 
     int matrixSize = 0;
+    bool unoptimized = false;
+    bool SIMD = true;
+    bool OMP = false;
+    bool MPI_OMP = false;
     
     int i = 0;
-    for (i; i < 2000; i = i + 50) {
+    for (i; i <= 2000; i = i + 50) {
 
         matrixSize = i;
         printf("Matrix size: %d\n", matrixSize);
 
         //initialize clock
-        clock_t start, end;
+        clock_t start, end, SIMDStart, SIMDEnd, OMPStart, OMPEnd;
         int totalTime = 0;
 
         //allocate memory for target matrix calculation
@@ -33,36 +38,41 @@ int main(int argc, char *argv[]) {
         b = gen_matrix(matrixSize, matrixSize);
         double *c_calc = malloc(matrixSize * matrixSize * sizeof(double));
 
-        //time each matrix multiplication method
+        //Time Unoptimized
+        if(unoptimized) {
+            start = clock();
+            mmult(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
+            end = clock();
+            totalTime = (int)(end - start);
+            printf("Total time for mmult: %d\n", totalTime);
+        }
 
-        //Unoptimized
-        // start = clock();
-        // mmult(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
-        // end = clock();
-        // totalTime = (int)(end - start);
-        // printf("Total time for mmult: %d\n", totalTime);
+        //Time SIMD
+        if(SIMD) {
+            SIMDStart = clock();
+            mmult_simd(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
+            SIMDEnd = clock();
+            int SIMDTotalTime = (int)(SIMDEnd - SIMDStart);
+            printf("Total time for mmult_simd: %d\n", SIMDTotalTime);
+        }
 
-        //SIMD
-        start = clock();
-        mmult_simd(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
-        end = clock();
-        totalTime = (int)(end - start);
-        printf("Total time for mmult_simd: %d\n", totalTime);
+        //Time OMP
+        if(OMP) {
+            OMPStart = clock();
+            mmult_omp(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
+            OMPEnd = clock();
+            int OMPTotalTime = (int)(OMPEnd - OMPStart);
+            printf("Total time for mmult_omp: %d\n", OMPTotalTime);
+        }
 
-        //OMP
-        start = clock();
-        mmult_omp(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
-        end = clock();
-        totalTime = (int)(end - start);
-        printf("Total time for mmult_omp: %d\n", totalTime);
-
-        /*
-        start = clock();
-        mmult_mpi_omp(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
-        end = clock();
-        totalTime = (double)(end - start);
-        printf("Total time for mmult_simd: %f\n", totalTime);
-        */
+        //Time MPI OMP
+        if(MPI_OMP) {
+            start = clock();
+            //mmult_mpi_omp(c_calc, a, matrixSize, matrixSize, b, matrixSize, matrixSize);
+            end = clock();
+            int MPI_OMPTotalTime = (double)(end - start);
+            printf("Total time for mmult_simd: %d\n", MPI_OMPTotalTime);
+        }
 
         // compare each matrix without timer
         //compare_matrices(c_actual, mmultTarget, matrixSize, matrixSize);
