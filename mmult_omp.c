7c0d6f0151e3c1c9771d3eade4e896e402d966b1
dariodaguanno <tul11082@temple.edu>
2023-03-19 20:11:34 -0400

    Inserted code in mmult_omp.c for matrix multiplication with openMP

--- mmult_omp.c
+++ mmult_omp.c
@@ -13,13 +13,23 @@
  * @return 0 if the matrix multiplication is successful.
  */
 int mmult_omp(double *c,
-		      double *a, int aRows, int aCols,
-		      double *b, int bRows, int bCols) {
+		      const double *a, int aRows, int aCols,
+		      const double *b, int bRows, int bCols) {
 
   // Insert your code here
-
-  
-    
+    int i, j, k;
+#pragma omp parallel default(none) shared(a, b, c, aRows, aCols, bRows, bCols) private(i, k, j)
+#pragma omp for
+    for (i = 0; i < aRows; i++) {
+        for (j = 0; j < bCols; j++) {
+            c[i*bCols + j] = 0;
+        }
+        for (k = 0; k < aCols; k++) {
+            for (j = 0; j < bCols; j++) {
+                c[i*bCols + j] += a[i*aCols + k] * b[k*bCols + j];
+            }
+        }
+    }
     return 0;
 }
 
