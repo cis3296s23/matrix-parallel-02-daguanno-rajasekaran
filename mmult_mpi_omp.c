c76b388b3b434f1b5b82a65c3aab4d51f492e3f8
Alexander Rajasekaran <50925468+triplehamburger@users.noreply.github.com>
2023-03-21 00:05:08 -0400

    2

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -70,7 +70,7 @@ int main(int argc, char* argv[])
             MPI_Bcast(bb, nrows * ncols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
             //for loop to send each stripe to a slave
-            for(k = 0; k < 4; k++) {
+            for(k = 0; k < 3; k++) {
             printf("earn your stripes\n");
 
                 for (i = 0; i < stripesize; i++) {
