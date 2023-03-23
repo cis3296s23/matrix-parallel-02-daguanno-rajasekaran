ba76f0871d9487a3f1fd94b79f006e6e8f5d0f8f
Alexander Rajasekaran <50925468+triplehamburger@users.noreply.github.com>
2023-03-23 12:02:13 -0400

    24

--- script.sh
+++ script.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Compile gen_gnu.c and gen_gnu_O3.c
+echo "compiling gen_gnu's"
 gcc -o gen_gnu gen_gnu.c
 gcc -o gen_gnu_O3 -O3 gen_gnu_O3.c
 
 # Compile mmult_mpi.c with MPI
+echo "compiling mpi"
 mpicc -o mmult_mpi -fopenmp -O3 mmult.o mmult_mpi_omp.o mat.c
 
 # Compile mmult_mpi_omp.c with MPI and OpenMP
+echo "compiling mpi omp"
 mpicc -o mmult_mpi_omp -fopenmp -O3 mmult.o mmult_mpi_omp.o mat.c
 
 # Run the programs
+echo "running gen_gnu's"
 ./gen_gnu >> data.txt
 ./gen_gnu_O3 >> data.txt
 
